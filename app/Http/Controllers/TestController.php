edf1d0683027c68f1b5e0b5f8bd8171038710db2
dna20021225 <dna20021225@gmail.com>
2024-10-16 17:52:23 +0900

    各種書類用のコントローラーの雛形を用意

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -22,9 +22,50 @@ class TestController extends Controller
 
         $documentName = $categoriesWithItems[$category][$id];
         
-        // ここで実際の文書処理ロジックを実装します
-        // 例: return view('documents.show', compact('documentName'));
-        
-        return "表示: {$category}カテゴリの{$documentName}";
+        // カテゴリーとIDに基づいて適切なビューを返す
+        switch ($documentName) {
+            case '経費申請書':
+                return view('documents.application_for_expenses');
+            case '通勤手当申請書':
+                return view('documents.commute_allowance_application');
+            case '駐在旅費仮払申請書':
+                return view('documents.travel_expense_advance_application');
+            case '稟議書':
+                return view('documents.resolution');
+            case '慶弔見舞申請書':
+                return view('documents.congratolatory_payment_application');
+            case '資格申請書':
+                return view('documents.qualification_application');
+            case '出張旅費申請書':
+                return view('documents.travel_expense_application');
+            case '通勤費精算書':
+                return view('documents.commute_expense_statement');
+            case '年末年始手当申請書':
+                return view('documents.year_end_and_new_year_allowance_application');
+            case '旅費支給申請書':
+                return view('documents.travel_expense_payment_application');
+            case '月間作業報告書':
+                return view('documents.monthly_work_report');
+            case '週間作業報告書':
+                return view('documents.weekly_work_report');
+            case '社外研修報告書':
+                return view('documents.external_training_report');
+            case '出張計画書':
+                return view('documents.business_trip_plan');
+            case '出張報告書':
+                return view('documents.business_trip_report');
+            case '休職届':
+                return view('documents.leave_of_absence_notice');
+            case '自己申請書':
+                return view('documents.self_application');
+            case '作業確認20日〆':
+                return view('documents.work_confirmation_20th');
+            case '作業確認31日〆':
+                return view('documents.work_confirmation_31st');
+            case '勤務予測':
+                return view('documents.work_forecast');
+            default:
+                abort(404);
+        }
     }
 }
