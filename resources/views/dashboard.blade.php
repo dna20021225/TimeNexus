e6a53919b7f8f25bd9b82e76d11028ccf8c7292f
dna20021225 <dna20021225@gmail.com>
2024-10-20 16:23:48 +0900

    ダッシュボードに所属を追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,12 @@
 
     <div class="py-12 bg-gray-100">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+
+            <x-department-component :department="'デジタルエンタテイメント事業部'" :username="auth()->user()->name"/>
+
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="mb-4 text-lg font-semibold">勤怠管理</h3>
+                    <h3 class="mb-4 text-lg font-semibold">勤怠入力</h3>
                     <div class="flex space-x-4">
                         <a href="{{ route('dashboard') }}" class="px-6 py-2 text-white transition duration-300 ease-in-out bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">出勤</a>
                         <a href="{{ route('dashboard') }}" class="px-6 py-2 text-white transition duration-300 ease-in-out bg-green-500 rounded-full hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50">退勤</a>
@@ -19,7 +22,7 @@
 
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <h3 class="mb-4 text-lg font-semibold">カテゴリー選択</h3>
+                    <h3 class="mb-4 text-lg font-semibold">各種書類</h3>
                     <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                         @foreach ($categoriesWithItems as $category => $items)
                             <div class="relative">
@@ -38,10 +41,13 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h3 class="mb-4 text-lg font-semibold">検索</h3>
-                    
+                    <form action="{{ route('dashboard') }}" method="GET">
+                        @csrf
+                        <textarea name="query" class="w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" placeholder="検索ワードを入力してください"></textarea>
+                        <button type="submit" class="w-full px-4 py-2 mt-4 text-white transition duration-300 ease-in-out bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">検索</button>
+                    </form>
                 </div>
             </div>
-            
         </div>
     </div>
 </x-app-layout>
