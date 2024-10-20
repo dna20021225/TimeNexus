1cec0db2a97084de3a46beec582ec9ea6fcb007a
dna20021225 <dna20021225@gmail.com>
2024-10-20 17:57:44 +0900

    各種書類に所属コンポーネントを追加

--- resources/views/documents/application_for_expenses.blade.php
+++ resources/views/documents/application_for_expenses.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12 bg-gray-100">
         <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+            <x-department-component :department="'デジタルエンタテイメント事業部'" :username="auth()->user()->name"/>
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     <h3 class="mb-4 text-lg font-semibold"></h3>
