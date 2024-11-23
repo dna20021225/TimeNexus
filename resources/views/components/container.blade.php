81d641254bbe5cca9d3afead044eaffa2649ca2b
dna20021225 <dna20021225@gmail.com>
2024-11-23 12:17:57 +0900

    コンテナサイズの指定を削除

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,5 +1,5 @@
 <div class="py-3 bg-gray-200">
-    <div class="h-screen max-w-full mx-auto space-y-3 sm:px-2 lg:px-3">
+    <div class="max-w-full mx-auto space-y-3 sm:px-2 lg:px-3">
         {{ $slot }}
     </div>
 </div>
